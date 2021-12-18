e0abc9203461fc8e6da76dc1dc1a2e66c9e19378
unknown <manuelfrancoesquivel@gmail.com>
2021-12-17 20:00:32 -0600

    ajuste front

--- js/app.js
+++ js/app.js
@@ -1,5 +1,15 @@
 var content = document.getElementById('content');
 
+var isLoad = document.getElementById('isLoad');
+
+function isNotLoading(){
+    $( document ).ready(function() {
+        $("#load").fadeIn(500);  
+    });
+    $( document ).ready(function() {
+        $("#load").fadeOut(800);  
+    });
+}
 
 function datos(){
     var select = document.getElementById('select').value;
@@ -8,30 +18,37 @@ function datos(){
     switch (select) {
         case '1':
             content.innerHTML = '';
+            isNotLoading();
             consumo('posts');
             break;
         case '2':
             content.innerHTML = '';
+            isNotLoading();
             consumo('comments');
             break;
         case '3':
             content.innerHTML = '';
+            isNotLoading();
             consumo('albums');
             break;
         case '4':
             content.innerHTML = '';
+            isNotLoading();
             consumo('photos');
             break;
         case '5':
             content.innerHTML = '';
+            isNotLoading();
             consumo('todos');
             break;
         case '6':
             content.innerHTML = '';
+            isNotLoading();
             consumo('users');
             break;
         default:
             content.innerHTML = 'Ha ocurrido un error o no haz seleccionado nada';
+            isNotLoading();
             break;
     }
 }
@@ -47,11 +64,13 @@ function consumo(fuente){
                     data.forEach(element => {
                         content.innerHTML += 
                         `
-                        <div class="col-sm-12 col-md-4 d-flex justify-content-center p-4">
+                        <div class="col-sm-12 col-md-4 d-flex justify-content-center p-3">
                             <div class="card">
                                 <div class="card-body">
                                     <div class="card-title"><b>${element.title}</b></div>
                                     <div class="card-text">${element.body}</div>
+                                </div>
+                                <div class="card-footer">
                                     <ul class="list-group list-group-flush">
                                         <li class="list-group-item">ID: ${element.id}</li>
                                         <li class="list-group-item">ID de usuario: ${element.userId}</li>
@@ -66,7 +85,7 @@ function consumo(fuente){
                     data.forEach(element => {
                         content.innerHTML += 
                         `
-                        <div class="col-sm-12 col-md-4 d-flex justify-content-center p-4">
+                        <div class="col-sm-12 col-md-4 d-flex justify-content-center p-3">
                             <div class="card">
                                 <div class="card-header">
                                     ${element.email}
@@ -74,6 +93,8 @@ function consumo(fuente){
                                 <div class="card-body">
                                     <div class="card-title"><b>${element.name}</b></div>
                                     <div class="card-text">${element.body}</div>
+                                </div>
+                                <div class="card-footer">
                                     <ul class="list-group list-group-flush">
                                         <li class="list-group-item">ID: ${element.id}</li>
                                         <li class="list-group-item">ID de comentario: ${element.postId}</li>
@@ -88,10 +109,12 @@ function consumo(fuente){
                     data.forEach(element => {
                         content.innerHTML += 
                         `
-                        <div class="col-sm-12 col-md-4 d-flex justify-content-center p-4">
+                        <div class="col-sm-12 col-md-4 d-flex justify-content-center p-3">
                             <div class="card">
                                 <div class="card-body">
                                     <div class="card-title"><b>${element.title}</b></div>
+                                </div>
+                                <div class="card-footer">
                                     <ul class="list-group list-group-flush">
                                         <li class="list-group-item">ID: ${element.id}</li>
                                         <li class="list-group-item">ID de usuario: ${element.userId}</li>
@@ -109,11 +132,13 @@ function consumo(fuente){
                         }
                         content.innerHTML += 
                         `
-                        <div class="col-sm-12 col-md-4 d-flex justify-content-center p-4">
+                        <div class="col-sm-12 col-md-4 d-flex justify-content-center p-3">
                             <div class="card">
                                 <img src="${element.thumbnailUrl}" class="card-img-top" alt="..." style="width:100%;">
                                 <div class="card-body">
                                     <div class="card-title"><b>${element.title}</b></div>
+                                </div>
+                                <div class="card-footer">
                                     <ul class="list-group list-group-flush">
                                         <li class="list-group-item">ID: ${element.id}</li>
                                         <li class="list-group-item">ID de album: ${element.albumId}</li>
@@ -129,20 +154,22 @@ function consumo(fuente){
                     data.forEach(element => {
                         
                         if (!element.completed) {
-                            typeCard = `<div class="card text-white bg-danger">`;
+                            typeCard = `Completado: <span class="badge bg-danger">${element.completed}</span>`;
                         }else{
-                            typeCard = `<div class="card text-white bg-success">`;
+                            typeCard = `Completado: <span class="badge bg-success">${element.completed}</span>`;
                         }
 
                         content.innerHTML += 
                         `
-                        <div class="col-sm-12 col-md-4 d-flex justify-content-center p-4">`
+                        <div class="col-sm-12 col-md-4 d-flex justify-content-center p-3">
+                            <div class="card">
+                                <div class="card-header">`
                         +typeCard+`
-                                <div class="card-header text-white">
-                                    Completado: ${element.completed}
                                 </div>
                                 <div class="card-body">
                                     <div class="card-title"><b>${element.title}</b></div>
+                                </div>
+                                <div class="card-footer">
                                     <ul class="list-group list-group-flush">
                                         <li class="list-group-item">ID: ${element.id}</li>
                                         <li class="list-group-item">ID de usuario: ${element.userId}</li>
@@ -157,7 +184,7 @@ function consumo(fuente){
                     data.forEach(element => {
                         content.innerHTML += 
                         `
-                        <div class="col-sm-12 col-md-4 d-flex justify-content-center p-4">
+                        <div class="col-sm-12 col-md-4 d-flex justify-content-center p-3">
                             <div class="card">
                                 <div class="card-header">
                                    ID: ${element.id}
@@ -165,6 +192,8 @@ function consumo(fuente){
                                 <div class="card-body">
                                     <div class="card-title">Nombre: <b>${element.name}</b></div>
                                     <div class="card-text"> ${element.email}</div>
+                                </div>
+                                <div class="card-footer">
                                     <ul class="list-group list-group-flush">
                                         <li class="list-group-item">Nombre de usuario: ${element.username}</li>
                                         <li class="list-group-item">Direccion: ${element.address.street}, ${element.address.city}, ${element.address.zipcode}</li>
